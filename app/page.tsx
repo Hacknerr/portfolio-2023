4bc83e150150a679eda119db00c06bbff3fc46d4
Hacknerr <andre-g96@hotmail.com>
2023-05-25 21:50:22 +0200

    added vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
+import { Analytics } from '@vercel/analytics/react'
+
 export default function Home() {
   return (
       <>
@@ -20,6 +22,8 @@ export default function Home() {
           <Projects />
           <Contact />
           <Footer />
+
+          <Analytics />
 
       </>
   )
